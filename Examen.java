51aac3ae3102b2108e158607c1c88697603e4175
aroig3 <101899182+aroig3@users.noreply.github.com>
2022-04-08 19:26:35 +0200

    Update and rename examen.java to Examen.java

--- Examen.java
+++ Examen.java
@@ -1,4 +1,4 @@
-public class Prova {
+public class Examen {
    public static void main(String[] args) {
       System.out.println("Soc l'Arnau Roig Boada i estic fent l'examen de la UF2 del M01 de DAM");
    }
